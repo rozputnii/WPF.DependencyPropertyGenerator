3d79cc61bb178874c7a0bffa4212a620a4b19018
Daniil Rozputnii <daniil.rozputnii@gmail.com>
2024-05-25 00:56:18 +0100

    net8.0 single TargetFramework

---
+++ src/libs/H.Generators.Extensions/Framework.cs
@@ -0,0 +1,47 @@
+﻿namespace H.Generators.Extensions;
+
+/// <summary>
+/// Framework enumeration for generators that are platform specific.
+/// </summary>
+public enum Framework
+{
+    /// <summary>
+    /// Not recognized.
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// Windows Presentation Foundation.
+    /// </summary>
+    Wpf,
+
+    /// <summary>
+    /// Universal Windows Platform.
+    /// </summary>
+    Uwp,
+
+    /// <summary>
+    /// WinUI 3/WindowsAppSDK.
+    /// </summary>
+    WinUi,
+
+    /// <summary>
+    /// Uno.
+    /// </summary>
+    Uno,
+
+    /// <summary>
+    /// Uno WinUI.
+    /// </summary>
+    UnoWinUi,
+
+    /// <summary>
+    /// AvaloniaUI.
+    /// </summary>
+    Avalonia,
+
+    /// <summary>
+    /// MAUI.
+    /// </summary>
+    Maui,
+}
